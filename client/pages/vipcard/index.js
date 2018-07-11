4fe56993133b41c0ba16ef4bc92289114c04aaee
hackerslizc <hackerslizc@gmail.com>
2018-07-12 02:31:53 +0800

    会员升级&会员卡

--- client/pages/vipcard/index.js
+++ client/pages/vipcard/index.js
@@ -5,62 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    isLogined: false,
+    viptype: 'gold'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.setNavigationBarTitle({
+      title: '会员卡',
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  userBtnEventFn(e){
+    const { viptype, isLogined} = this.data;
+    if (isLogined) {
+      wx.navigateTo({
+        url: '/pages/vipupgraded/index',
+      })
+    } else {
+      //未登陆用户不知道逻辑暂时跳转到绑定页面
+      wx.navigateTo({
+        url: '/pages/bindphone/index',
+      })
+    }
   }
 })
