c2bf22ac3de9f8b52c70e51ed21c772685140cb4
hackerslizc <hackerslizc@gmail.com>
2018-07-14 02:50:28 +0800

    支付

--- client/components/pay-item/index.js
+++ client/components/pay-item/index.js
@@ -18,14 +18,68 @@ Component({
    * 可用于模版渲染
    */
   data: {
-    // 弹窗显示控制
-    isShow: false
+    allinputF1: true,
+    pwdArr: [],
+    inputStr: '',
+    focusF: [],
+    allinput: '',
   },
   /**
    * 组件的方法列表
    * 更新属性和数据的方法与更新页面数据的方法类似
    */
   methods: {
+    passInput(e) {
+      const dType = e.target.dataset.type;
+      console.log('dType: ', dType)
+      const value = e.detail.value;
+      const num = e.detail.cursor - 1;
+      const tempArr = [];
+      let data = '';
+      for (let i = 0; i <= num; i++) {
+        tempArr.push(value.substring(i, i + 1))
+      }
+      if (tempArr.length >= 6) {
+        data = {
+          inputSrr: value,
+          allinput: value,
+          pwdArr: tempArr
+        }
+      } else {
+        data = {
+          inputSrr: value,
+          allinput: '',
+          pwdArr: tempArr
+        }
+      }
+      console.log(`${dType}密码输入完成: ${tempArr}`)
+      this.setData(data)
+    },
+    allInputFn(e) {
+      const index = e.target.dataset.index;
+      console.log(index)
+      // var focusF = http.forC(that.data.focusF);
+      var pwdArr = that.data.pwdArr;
+      var value = e.detail.value
+      if (pwdArr[index]) {
+        pwdArr[index] = '';
+        focusF[index] = '1';
+      } else {
+        pwdArr[index] = value;
+        focusF[index + 1] = '1';
+      }
+      this.setData({
+        focusF: focusF,
+        pwdArr: pwdArr
+      })
+      console.log(index)
+    },
+    focusInput(e) {
+      console.log(e.target.dataset)
+      this.setData({
+        allinputF1: true
+      })
+    },
 
   }
 })
